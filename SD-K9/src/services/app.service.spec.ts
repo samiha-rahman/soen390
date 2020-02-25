6a73d29e760e33a5d5355434ac372a8013a4ad5c
Weigiu <michael842055@hotmail.com>
2020-02-24 22:12:52 -0500

    unit test for collapsed menu

--- SD-K9/src/services/app.service.spec.ts
+++ SD-K9/src/services/app.service.spec.ts
@@ -1,15 +1,26 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-
 import { AppService } from './app.service';
+import { IonicPullupModule } from 'ionic-pullup';
+import { IonPullUpFooterState } from 'ionic-pullup';
+
+let Pullup = null;
 
 describe('AppService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    schemas:[NO_ERRORS_SCHEMA]
-  }));
+  beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should do nothing', () => {
-    //const service: AppService = TestBed.get(AppService);
-    expect(undefined).toBeUndefined();
+    const service: AppService = TestBed.get(AppService);
+    expect(true).toBeTruthy();
+  });
+});
+
+describe('IonPullupModule', ()=>{
+  beforeEach(() => {
+    Pullup = new IonicPullupModule();  
+  });
+
+  it("Menu should be collapsed", () => {
+    Pullup = IonPullUpFooterState.Collapsed;
+    expect(Pullup).toEqual(IonPullUpFooterState.Collapsed)
   });
 });
