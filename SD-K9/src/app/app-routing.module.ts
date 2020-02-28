87db7cb5d2e52093b32cab97c5f5fd0b19d6486b
Weigiu <35940338+Weigiu@users.noreply.github.com>
2020-02-27 23:20:51 -0500

    fixed json error, remove 2nd block and add file

--- SD-K9/src/app/app-routing.module.ts
+++ SD-K9/src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  
+  { path: 'home', 
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
   {
     path: 'bus',
     loadChildren: () => import('./modals/bus/bus.module').then( m => m.BusPageModule)
@@ -13,18 +14,6 @@ const routes: Routes = [
     path: 'appsettings',
     loadChildren: () => import('./modals/appsettings/appsettings.module').then( m => m.AppsettingsPageModule)
   },
-
-];
-
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
-})
-const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)},
 ];
 
 @NgModule({
