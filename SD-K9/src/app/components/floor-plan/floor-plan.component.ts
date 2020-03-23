ad05f4f340ff20be459a0462847e506d4fb9923f
Joanna Lin <33985435+jayohanna@users.noreply.github.com>
2020-03-23 15:16:21 -0400

    #31 Added svg-pan-zoom
    Added svg-pan-zoom library to enable panning and zooming of the indoor map

--- SD-K9/src/app/components/floor-plan/floor-plan.component.ts
+++ SD-K9/src/app/components/floor-plan/floor-plan.component.ts
@@ -7,6 +7,7 @@ import {
 import { SVG } from "@svgdotjs/svg.js";
 
 import { SVGManager } from '../../providers/svg-manager.service';
+import { svgPanZoom } from 'svg-pan-zoom/src/svg-pan-zoom.js';
 import { Map } from 'src/app/interfaces/map';
 
 @Component({
@@ -20,19 +21,32 @@ export class FloorPlanComponent implements OnInit, OnDestroy, Map {
   floor: number;
 
   private _draw;
+  private _panZoomInstance;
 
   constructor(private _svgManager: SVGManager) {}
 
   ngOnInit() {
     this._drawFloorplan(this.data.building, this.data.floor);
+    this._setPanZoom();
   }
 
   private _drawFloorplan(building: string, floor: number) {
     this._svgManager.getSVG(`${building}/${floor}`).subscribe(floorplan => {
       /* find the svg container, clear it and replace with new floorplan */
-      this._draw = SVG("#floorplan");
+      this._draw = SVG('#floorplan');
       this._draw.clear();
       this._draw.svg(floorplan);
+    });
+  }
+
+  private _setPanZoom() {
+    this._panZoomInstance = svgPanZoom('#floorplan', {
+      zoomEnabled: true,
+      controlIconsEnabled: true,
+      fit: true,
+      center: true,
+      minZoom: 0.5,
+      maxZoom: 2
     });
   }
 
