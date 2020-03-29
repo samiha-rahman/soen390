2115c33337b30e5ba87ba20b5b3b0cef89d4276e
Paul Musat <paul.musat@mail.concordia.ca>
2020-03-29 18:32:32 -0400

    #31 Removed trailing whitespace from vertical transportation enum

--- SD-K9/src/app/models/vertical-transport.enum.model.ts
+++ SD-K9/src/app/models/vertical-transport.enum.model.ts
@@ -1,5 +1,5 @@
 export enum VerticalTransport {
-    ESCALATOR = 'ESCALATOR ',
-    ELEVATOR = 'ELEVATOR ',
-    STAIRS = 'STAIRS '
+    ESCALATOR = 'ESCALATOR',
+    ELEVATOR = 'ELEVATOR',
+    STAIRS = 'STAIRS'
 }
