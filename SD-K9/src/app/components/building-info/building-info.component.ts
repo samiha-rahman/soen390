samiha-rahman/soen390

commit 5413d081fe4d639040269a371f9497c659cc12c8

Update SD-K9/src/app/components/building-info/building-info.component.ts
avoid big switch case when going inside

Co-Authored-By: paulMoose <40539079+paulMoose@users.noreply.github.com>

SD-K9/src/app/components/building-info/building-info.component.ts
@@ -77,15 +77,7 @@ export class BuildingInfoComponent implements OnInit, OnDestroy {
   }
 
   goInside(buildingSlug: string) {
-    switch(buildingSlug){
-      case "hall":{
-          this._mapModeStore.setMode(ViewMode.HALL);
-          break;
-      }
-      case "cc":{
-          this._mapModeStore.setMode(ViewMode.LOYOLA);
-          break;
-      }
+    this._mapModeStore.setMode(ViewMode[buildingSlug.toUpperCase()]);
     }
   }
 

commit 2b816e97ae8d9d1650fedd794f172452ed861e8d

fix typo introduced in 5413d081fe4d639040269a371f9497c659cc12c8

SD-K9/src/app/components/building-info/building-info.component.ts
@@ -78,7 +78,6 @@ export class BuildingInfoComponent implements OnInit, OnDestroy {
 
   goInside(buildingSlug: string) {
     this._mapModeStore.setMode(ViewMode[buildingSlug.toUpperCase()]);
-    }
   }
 
   //for testing