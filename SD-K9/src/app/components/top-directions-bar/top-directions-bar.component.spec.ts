2bebd8582288fecf7b2c0f7e1d727417a6cf6ffd
Alexandria Paggabao <a.paggabao@hotmail.com>
2020-03-29 18:04:10 -0400

    #31 Add unit tests for top-directions-bar

--- SD-K9/src/app/components/top-directions-bar/top-directions-bar.component.spec.ts
+++ SD-K9/src/app/components/top-directions-bar/top-directions-bar.component.spec.ts
@@ -4,12 +4,12 @@ import { IonSearchbar, IonBackButton, IonButton, IonButtons, IonIcon, IonGrid, I
 import { TopDirectionsBarComponent } from './top-directions-bar.component';
 import { FormsModule } from '@angular/forms';
 import { DirectionFormStore } from 'src/app/providers/state-stores/direction-form-store.service';
-import { Observable } from 'rxjs';
+import { Transport } from 'src/app/models/transport.enum.model';
+
 
 describe('TopDirectionsBarComponent', () => {
   let component: TopDirectionsBarComponent;
   let fixture: ComponentFixture<TopDirectionsBarComponent>;
-  let observable = new Observable<any>();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -33,7 +33,7 @@ describe('TopDirectionsBarComponent', () => {
       imports: [FormsModule],
       providers: [
         { provide: NavController, useValue: { load: jasmine.createSpy('load').and.returnValue(new Promise(() => true)) } },
-        { provide: DirectionFormStore, useValue: observable }
+        DirectionFormStore
       ]
     }).compileComponents();
 
@@ -45,4 +45,57 @@ describe('TopDirectionsBarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("#transportSegmentChanged should call `sendDirection()` whenever a trasnport mode is selected from ui", () => {
+    /* prepare DirectionForm */
+    component.start = 'h-815';
+    component.end = 'h-816';
+    component.transport = Transport.TRANSIT;
+
+    component.directionSent = true;
+    let event: Event = new CustomEvent("ion-segment", {
+      detail: {
+        value: "WALKING",
+      }
+    });
+
+    spyOn(component, 'sendDirection');
+    component.transportSegmentChanged(event);
+    expect(component.sendDirection).toHaveBeenCalled();
+  })
+
+  it('#verticalTransportSegmentChanged should call `sendDirection()` whenever a vertical-trasnport mode is selected from ui', () => {
+    /* prepare DirectionForm */
+    component.start = 'h-815';
+    component.end = 'h-816';
+    component.transport = Transport.TRANSIT;
+
+    component.directionSent = true;
+    let event: Event = new CustomEvent("ion-segment", {
+      detail: {
+        value: "ELEVATOR",
+      }
+    });
+
+    spyOn(component, 'sendDirection');
+    component.verticalTransportSegmentChanged(event);
+    expect(component.sendDirection).toHaveBeenCalled();
+  });
+
+  it('#sendDirection should send `true` with the `EventEmmitter`: `formCompleteEvent` when `DirectionForm`  is complete', () => {
+    /* prepare DirectionForm */
+    component.start = 'h-815';
+    component.end = 'h-816';
+    component.transport = Transport.TRANSIT;
+
+    spyOn(component.formCompleteEvent, 'emit');
+    component.sendDirection();
+    expect(component.formCompleteEvent.emit).toHaveBeenCalledWith(true);
+  });
+
+  it('#clearDirection should send `false` with the `EventEmmitter`: `formCompleteEvent`', () => {
+    spyOn(component.formCompleteEvent, 'emit');
+    component.clearDirection()
+    expect(component.formCompleteEvent.emit).toHaveBeenCalledWith(false);
+  })
 });
