98d9022bc95d14715028dbc236301daf1c6e074b
Samiha <hellosamiha@gmail.com>
2020-03-29 22:37:57 -0400

    remove unused tags and method #83

--- SD-K9/e2e/src/top-directions-bar.po.ts
+++ SD-K9/e2e/src/top-directions-bar.po.ts
@@ -6,14 +6,6 @@ export class TopDirectionsBarComponent extends PageObject {
         super('top-directions-bar');
     }
 
-    // enterStart(start: string) {
-    //     this.enterInputText(' ion-searchbar#start-searchbar', start);
-    // }
-
-    // enterDestination(destination: string) {
-    //     this.enterInputText(' ion-searchbar#destination-searchbar', destination);
-    // }
-
     enterStart() {
         this.clickButton(' ion-searchbar#start-searchbar');
     }
