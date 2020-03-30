33cfdb2c349a1aa481b6ac09878fb600bae8dc19
Jack Leung <jack-_-leung@hotmail.com>
2020-03-29 23:11:20 -0400

    Trying to integrate Places API into location-search.page

--- SD-K9/src/app/pages/location-search/location-search.page.ts
+++ SD-K9/src/app/pages/location-search/location-search.page.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { DirectionFormStore } from '../../providers/state-stores/direction-form-store.service';
 import { ActivatedRoute } from '@angular/router';
+import { GoogleStore } from '../../providers/state-stores/google-store.service';
+import { UnsubscribeCallback } from '../../interfaces/unsubscribe-callback';
+
+declare var google;
 
 @Component({
   selector: 'app-location-search',
@@ -11,6 +15,7 @@ import { ActivatedRoute } from '@angular/router';
 export class LocationSearchPage implements OnInit {
 
   // @ViewChild(IonSearchbar) searchbar: IonSearchbar;
+  @ViewChild('searchbar', {read: ElementRef, static: true}) searchbar: ElementRef;
 
   query: string;
   itemList: string[];
@@ -20,14 +25,23 @@ export class LocationSearchPage implements OnInit {
 
   public currentQuery: string;
 
+  private _unsubscribeGoogleMap: UnsubscribeCallback;
+  map: any;
+
   constructor(
     private _navController: NavController,
     private _activatedRoute: ActivatedRoute,
-    private _directionFormStore: DirectionFormStore
+    private _directionFormStore: DirectionFormStore,
+    private _googleStore: GoogleStore
   ) {
     this._activatedRoute.queryParams.subscribe(params => {
       this._queryType = params['query'];
-    })
+    });
+    this._unsubscribeGoogleMap = this._googleStore.subscribe(() => {
+      this.map = this._googleStore.getGoogleMapState().map;
+      google = this._googleStore.getGoogleMapState().google;
+      this.searchplaces();
+    });
   }
 
   ngOnInit() {
@@ -63,4 +77,57 @@ export class LocationSearchPage implements OnInit {
     }
     this._navController.navigateBack("home");
   }
+
+  searchplaces(){
+    var input = this.searchbar.nativeElement.querySelector('.searchbar-input');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    // Bind the map's bounds (viewport) property to the autocomplete object,
+    // so that the autocomplete requests use the current map bounds for the
+    // bounds option in the request.
+    autocomplete.bindTo('bounds', this.map);
+    // Set the data fields to return when the user selects a place.
+    autocomplete.setFields(['address_components', 'geometry', 'icon', 'name']);
+    var infowindow = new google.maps.InfoWindow();
+    var infowindowContent = document.getElementById('infowindow-content');
+    infowindow.setContent(infowindowContent);
+    var marker = new google.maps.Marker({
+      map: this.map,
+      anchorPoint: new google.maps.Point(0, -29)
+    });
+    let map = this.map;
+    autocomplete.addListener('place_changed', function () {
+      infowindow.close();
+      marker.setVisible(false);
+      var place = autocomplete.getPlace();
+      if (!place.geometry) {
+        // User entered the name of a Place that was not suggested and
+        // pressed the Enter key, or the Place Details request failed.
+        window.alert("No details available for input: '" + place.name + "'");
+        return;
+      }
+      // If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      }
+      else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+      }
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+      var address = '';
+      if (place.address_components) {
+        address = [
+          (place.address_components[0] && place.address_components[0].short_name || ''),
+          (place.address_components[1] && place.address_components[1].short_name || ''),
+          (place.address_components[2] && place.address_components[2].short_name || '')
+        ].join(' ');
+      }
+      infowindowContent.children['place-icon'].src = place.icon;
+      infowindowContent.children['place-name'].textContent = place.name;
+      infowindowContent.children['place-address'].textContent = address;
+      infowindow.open(map, marker); // Display the information of marker
+    });
+  }
+
 }
