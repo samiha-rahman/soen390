b079501022ec992998d8b0d61dff712ad43129d8
ThierryRomain <thierryr@orphic.ca>
2020-04-12 13:24:44 -0400

    Added near me functionality in pullup bar #15

--- SD-K9/src/app/components/pullup/pullup.component.ts
+++ SD-K9/src/app/components/pullup/pullup.component.ts
@@ -3,6 +3,10 @@ import { IonPullUpFooterState} from 'ionic-pullup';
 import { ModalController } from '@ionic/angular';
 import { AppSettings } from 'src/app/pages/app-settings/app-settings.page';
 import { BusPage} from 'src/app/pages/bus/bus.page';
+import { GoogleStore } from '../../providers/state-stores/google-store.service';
+import { UnsubscribeCallback } from '../../interfaces/unsubscribe-callback';
+
+declare var google;
 
 @Component({
   selector: 'app-pullup',
@@ -11,11 +15,17 @@ import { BusPage} from 'src/app/pages/bus/bus.page';
 })
 export class PullupComponent implements OnInit {
   footerState: IonPullUpFooterState;
+  private _unsubscribe: UnsubscribeCallback;
+  private _googleAutocomplete: any;
+  private _nearMeMarkers:any[] = [];
 
 
 
-
-  constructor(public modalController: ModalController) { }
+constructor(public modalController: ModalController, private _googleStore: GoogleStore) {
+  this._unsubscribe = this._googleStore.subscribe(() => {
+    google = this._googleStore.getGoogleMapState().google;
+  });
+}
 
   async presentModal() {
       const modal = await this.modalController.create({
@@ -50,5 +60,53 @@ export class PullupComponent implements OnInit {
     // toggle footer states
     toggleFooter() {
         this.footerState = this.footerState === IonPullUpFooterState.Collapsed ? IonPullUpFooterState.Expanded : IonPullUpFooterState.Collapsed;
+    }
+
+    private _placeNearMeMarkers(request,map){
+      //clear existing markers
+      for(let i = 0; i < this._nearMeMarkers.length; i++){
+        this._nearMeMarkers[i].setMap(null);
+      }
+      this._nearMeMarkers = [];
+
+      let places = new google.maps.places.PlacesService(map);
+      places.nearbySearch(request, (results, status) => {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            let infowindow = new google.maps.InfoWindow({
+              content: "<h3>" + results[i].name + "</h3><br><p>Rating : " + results[i].rating + " </p>"
+            });
+            let image = {
+              url: results[i].icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+            let marker = new google.maps.Marker({
+              map: map,
+              icon: image,
+              title: results[i].name,
+              position: results[i].geometry.location
+            });
+            marker.addListener('click', function() {
+              infowindow.open(map, marker);
+            });
+            this._nearMeMarkers.push(marker);
+          }
+        }
+      });
+    }
+
+    showNearMe(type : string){
+      let map = this._googleStore.getGoogleMapState().map;
+      let request = {
+        location: map.getCenter(),
+        radius: '500',
+        type: [type]
+      };
+
+      this._placeNearMeMarkers(request,map);
+      this.toggleFooter();
     }
 }
