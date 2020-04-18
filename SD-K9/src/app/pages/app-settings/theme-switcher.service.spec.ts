b6d16c07f2c8782f03524faa14a877b46505998d
Mack <trenholm.mackenzie@gmail.com>
2020-04-17 21:13:57 -0400

    color scheme arrangement

--- SD-K9/src/app/pages/app-settings/theme-switcher.service.spec.ts
+++ SD-K9/src/app/pages/app-settings/theme-switcher.service.spec.ts
@@ -25,35 +25,35 @@ export class ThemeSwitcherService {
       {
         name: 'day',
         styles: [
-          { themeVariable: '--ion-color-primary', value: '#f8383a'},
-          { themeVariable: '--ion-color-primary-rgb', value: '248,56,58'},
+          { themeVariable: '--ion-color-primary', value: '#A31B1B'},
+          { themeVariable: '--ion-color-primary-rgb', value: '163, 27, 27'},
           { themeVariable: '--ion-color-primary-contrast', value: '#ffffff'},
           { themeVariable: '--ion-color-primary-contrast-rgb', value: '255,255,255'},
-          { themeVariable: '--ion-color-primary-shade', value: '#da3133'},
-          { themeVariable: '--ion-color-primary-tint', value: '#f94c4e'},
+          { themeVariable: '--ion-color-primary-shade', value: '#681111'},
+          { themeVariable: '--ion-color-primary-tint', value: '#b66161'},
           { themeVariable: '--ion-item-ios-background-color', value: '#ffffff'},
           { themeVariable: '--ion-item-md-background-color', value: '#ffffff'},
           { themeVariable: '--ion-tabbar-background-color', value: '#fff'},
           { themeVariable: '--ion-tabbar-ios-text-color-active', value: '#000000'},
           { themeVariable: '--ion-tabbar-md-text-color-active', value: '#000000'},
-          { themeVariable: '--ion-background-color', value: '#f94c4e'}
+          { themeVariable: '--ion-background-color', value: '#ffffff'}
         ]
       },
       {
         name: 'night',
         styles: [
-          { themeVariable: '--ion-color-primary', value: '#222428'},
-          { themeVariable: '--ion-color-primary-rgb', value: '34,34,34'},
+          { themeVariable: '--ion-color-primary', value: '#1E90FF'},
+          { themeVariable: '--ion-color-primary-rgb', value: '30,144,255},
           { themeVariable: '--ion-color-primary-contrast', value: '#ffffff'},
           { themeVariable: '--ion-color-primary-contrast-rgb', value: '255,255,255'},
-          { themeVariable: '--ion-color-primary-shade', value: '#1e2023'},
-          { themeVariable: '--ion-color-primary-tint', value: '#383a3e'},
-          { themeVariable: '--ion-item-ios-background-color', value: '#717171'},
-          { themeVariable: '--ion-item-md-background-color', value: '#717171'},
-          { themeVariable: '--ion-tabbar-background-color', value: '#222428'},
-          { themeVariable: '--ion-tabbar-ios-text-color-active', value: '#ffffff'},
-          { themeVariable: '--ion-tabbar-md-text-color-active', value: '#ffffff'},
-          { themeVariable: '--ion-background-color', value: '#383838'}
+          { themeVariable: '--ion-color-primary-shade', value: '#6495ED'},
+          { themeVariable: '--ion-color-primary-tint', value: '#4682B4'},
+          { themeVariable: '--ion-item-ios-background-color', value: '#ffffff'},
+          { themeVariable: '--ion-item-md-background-color', value: '#ffffff'},
+          { themeVariable: '--ion-tabbar-background-color', value: '#fff'},
+          { themeVariable: '--ion-tabbar-ios-text-color-active', value: '#000000'},
+          { themeVariable: '--ion-tabbar-md-text-color-active', value: '#000000'},
+          { themeVariable: '--ion-background-color', value: '#ffffff'}
         ]
       }
     ]
