c970330d6535df1c30004ac43cfcc0c80dc27eb6
Mack <trenholm.mackenzie@gmail.com>
2020-04-17 22:20:31 -0400

    PRivacy policy update
    added readable privacy policy.

--- SD-K9/src/app/pages/app-settings/app-settings.page.ts
+++ SD-K9/src/app/pages/app-settings/app-settings.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ThemeSwitcherService } from './theme-switcher.service.spec';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
@@ -9,7 +10,7 @@ import { ThemeSwitcherService } from './theme-switcher.service.spec';
 })
 export class AppSettings implements OnInit {
 
-  constructor(private modalController: ModalController, public themeSwitcher: ThemeSwitcherService) { }
+  constructor(private modalController: ModalController, public themeSwitcher: ThemeSwitcherService, public alertController: AlertController) { }
 
   ngOnInit() {
   }
@@ -19,6 +20,17 @@ export class AppSettings implements OnInit {
 
   async changeTheme(){
     this.themeSwitcher.cycleTheme();
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
 }
