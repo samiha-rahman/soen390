6b4c945a989ac54a5cd1efcc21ef581f4077cf2c
Mack <trenholm.mackenzie@gmail.com>
2020-04-19 00:52:35 -0400

    Change set-up of privacy policy

--- SD-K9/src/app/pages/app-settings/app-settings.page.ts
+++ SD-K9/src/app/pages/app-settings/app-settings.page.ts
@@ -3,6 +3,7 @@ import { ModalController } from '@ionic/angular';
 import { ThemeSwitcherService } from './theme-switcher.service.spec';
 import { AlertController } from '@ionic/angular';
 
+
 @Component({
   selector: 'app-settings',
   templateUrl: './app-settings.page.html',
@@ -24,9 +25,19 @@ export class AppSettings implements OnInit {
 
   async presentAlert() {
     const alert = await this.alertController.create({
-      header: 'Alert',
-      subHeader: 'Privacy Policy',
-      message: 'It is NavConU\'s policy to respect your privacy regarding any information we may collect while operating our website. This Privacy Policy applies to www.NavConU.com (hereinafter, "us", "we", or "www.NavConU.com"). We respect your privacy and are committed to protecting personally identifiable information you may provide us through the Website. We have adopted this privacy policy ("Privacy Policy") to explain what information may be collected on our Website, how we use this information, and under what circumstances we may disclose the information to third parties. This Privacy Policy applies only to information we collect through the Website and does not apply to our collection of information from other sources.',
+      header: 'PrivacyPolicy',
+      subHeader: '',
+      message: `
+      It is NavConU\'s policy to respect your privacy regarding any information we may 
+      collect while operating our website. This Privacy Policy applies to the NavConU Application 
+    (hereinafter, "us", "we", or "NavConU Application"). We respect your privacy and are committed
+    to protecting personally identifiable information you may provide us through the Website. 
+    We have adopted this privacy policy ("Privacy Policy") to explain what information may be 
+    collected on our Website, how we use this information, and under what circumstances we may 
+    disclose the information to third parties. This Privacy Policy applies only to information 
+   we collect through the Website and does not apply to our collection of information from other 
+  sources.
+      `,
       buttons: ['OK']
     });
 
